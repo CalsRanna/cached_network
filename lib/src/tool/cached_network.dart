1ebeb239641f10ce620ca969da0a6c2e00c73bb7
CalsRanna <calsranna@gmail.com>
2022-10-20 16:55:00 +0800

    feat(cache network): add duration to determine reacquire data from network or not

--- lib/src/tool/cached_network.dart
+++ lib/src/tool/cached_network.dart
@@ -11,11 +11,16 @@ class CachedNetwork {
   Directory? cacheDirectory;
   final Dio _dio;
 
-  /// Allowed method is one of [GET, PUT, PATCH, DELETE].
+  /// Use to fetch data from network or cache file.
   ///
+  /// Allowed method is one of [GET, PUT, PATCH, DELETE].
   /// If method is null then the default method is [GET].
+  ///
+  /// If duration is null the the file will last forever, or should be reacquired
+  /// while duration from file created is longer than duration specified in the param.
   Future<String> fetch(
     String url, {
+    Duration? duration,
     String? method,
     bool reacquire = false,
   }) async {
@@ -29,22 +34,37 @@ class CachedNetwork {
     var exist = await file.exists();
     if (exist == false || reacquire == true) {
       await file.create(recursive: true);
-      var isRedirect = false;
-      String data;
-      do {
-        var response = await _dio.fetch(RequestOptions(
-          method: method,
-          path: url,
-        ));
-        if (response.isRedirect != null && response.isRedirect == true) {
-          isRedirect = true;
-        }
-        data = response.data;
-      } while (isRedirect);
+      var data = await _fetchFromNetwork(url, method);
       await file.writeAsString(data);
       return data;
     } else {
+      if (duration != null) {
+        var createdAt = file.statSync().changed;
+        var now = DateTime.now();
+        if (now.difference(createdAt).compareTo(duration) > 0) {
+          var data = await _fetchFromNetwork(url, method);
+          await file.writeAsString(data);
+          return data;
+        }
+      }
       return file.readAsString();
     }
+  }
+
+  /// Use to fetch data from network but do not expose.
+  Future<String> _fetchFromNetwork(String url, String? method) async {
+    var isRedirect = false;
+    String data;
+    do {
+      var response = await _dio.fetch(RequestOptions(
+        method: method,
+        path: url,
+      ));
+      if (response.isRedirect != null && response.isRedirect == true) {
+        isRedirect = true;
+      }
+      data = response.data;
+    } while (isRedirect);
+    return data;
   }
 }
